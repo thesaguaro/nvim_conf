f5b68a7a0866a30a3f127b05625fa954e5112836
saguaro3 <vanjacisco025@gmail.com>
2025-04-03 03:33:53 +0200

    config v0.2

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,21 +1,41 @@
+local Util = require("lazyvim.util")
+
 return {
-  "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
+
+  -- Add java to treesitter.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "java" })
+    end,
+  },
+
+  {
+    "nvim-java/nvim-java",
+    config = false,
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        opts = {
+          setup = {
+            jdtls = function()
+              require('java').setup({
+                java_test = {
+                  enable = Util.has("nvim-dap"),
+                },
+                java_debug_adapter = {
+                  enable = Util.has("nvim-dap"),
+                },
+                spring_boot_tools = {
+                  enable = false,
+                },
+                jdk = {
+                  auto_install = false,
+                },
+              })
+            end,
           },
-        },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              -- Your custom nvim-java configuration goes here
-            })
-          end,
         },
       },
     },
